5a747e78bd4d851c9e3fa41b1d8d38b6a8f64040
Rohit Shaw <carpediem212045@gmail.com>
2022-06-20 21:31:09 +0530

    Add files via upload

---
+++ Py-Lab/duckyuii.py
@@ -0,0 +1,9 @@
+#  How to print in py
+print('Cloning goes on !!')
+
+# Can print using '/"/'''
+print("o------")
+print('''  |||||''')
+
+# Evaluating the expression
+print('^' * 14)
---
+++ Py-Lab/var_inp.py
@@ -0,0 +1,49 @@
+# Py automatically detects the typo of var ------>
+rosogolla = 10
+fiest = 65.05
+potato_chips = 5.000000
+
+# Py Interpretor Executes from top to bottom
+rosogolla = 15
+
+dairyMilk = 'Too Chocolaty'
+naryal__Paani = True
+
+print('The price of rosogolla is ${rosogolla}', rosogolla)
+print('Dairy Milk tastes -', dairyMilk)
+print('''Naryal panni cream is better than Maaza -->''', naryal__Paani)
+print('\n')
+
+# ---------------------------------------------------------------------------------
+# Prbie
+patient = 'HOD'
+age = 56
+is_Nashedi = True
+print('Patient name:', patient, "of age:",
+      age, '''is Nashedi:''', is_Nashedi)
+print('\n')
+# _________________________________________________________________________________
+
+
+# Py Input ---->
+name = input('What is your name ? : ')
+print('Hy, I\'m '+name)  # Concatination
+
+# -------------------------------------------------------------------------------------
+# Probie_1
+pNmae = input('What\'s your name? : ')
+f_color = input('& Fav color: ')
+print("\""+pNmae+" likes "+f_color+" color.\"\n")
+
+# Probie_2
+birth_year = int(input('Birth year: '))
+print(type(birth_year))
+age = 2022 - birth_year
+print(type(age))
+print('Your current age is:', age)
+
+# Probie_3
+weight = float(input('Specify your Weight(lbs): '))
+kgs_conv = weight * 0.45
+print('Weight in Kgs:', kgs_conv)
+# _______________________________________________________________________________________
