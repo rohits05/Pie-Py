d7f077747d3fe872945df226929bde1d80b2dbec
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2022-10-10 09:41:31 +0530

    index.js
    Added index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+let a="hello";
+let b=8;
+let c=11;
+console.log(a+b+c);
+
+//2
+console.log(typeof(a+b+c));
+
+//3
+const map={
+    name:"Sourav",
+    age:100,
+    isPrincipal:true,
+}
+map.age=200;
+
+console.log(map)
